d04171dc543ea0bdc607e8db0d6b84d1806028c8
Joseado381 <josealonzo.jaa@gmail.com>
2026-04-06 22:12:23 -0300

    commit tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,21 @@
 
 int main()
 {
+    int entero;
+    int *pEntero;
+
     printf("Hola Mundo\n");
+
+    printf("Ingrese un numero entero:\n\t");
+    scanf("%d",&entero);
+
+    pEntero = &entero;
+
+    printf("\tEl continido del puntero es: %d\n",*pEntero);
+    printf("\tLa direccion de memoria almacenada por el puntero: %p\n",pEntero);
+    printf("\tLa direccion de memoria de la vareable: %p\n", &entero);
+    printf("\tLa direccion de memoria del puntero: %p\n", &pEntero);
+    printf("\tTamanio de memoria de la variable: %d \n", sizeof(entero));
 
     return 0;
 }
